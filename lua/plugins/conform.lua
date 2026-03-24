47e828eafe632f355b8ce920d8d751d22220381b
louis.blazejczak-boulegue <louis.blazejczak-boulegue@sysnav.fr>
2026-03-24 09:20:04 +0100

    feat(format): json formatting with dprint

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -3,7 +3,7 @@ return { -- Autoformat
   opts = {
     notify_on_error = false,
     format_on_save = {
-      timeout_ms = 500,
+      timeout_ms = 800,
       lsp_fallback = true,
     },
     formatters_by_ft = {
@@ -12,12 +12,27 @@ return { -- Autoformat
       cpp = { 'clang-format' },
       python = { 'ruff_format' },
       cmake = { 'cmake_format' },
+      json = { 'dprint' },
       -- Conform can also run multiple formatters sequentially
       -- python = { "isort", "black" },
       --
       -- You can use a sub-list to tell conform to run *until* a formatter
       -- is found.
       -- javascript = { { "prettierd", "prettier" } },
+    },
+    formatters = {
+      dprint = { -- make dprint use the default dprint config if one is not present in the project
+        prepend_args = function(self, ctx)
+          local local_config = vim.fs.find({ 'dprint.json', '.dprint.json' }, { path = ctx.dirname, upward = true })[1]
+          if local_config then
+            return {}
+          end
+          return {
+            '--config',
+            vim.fn.expand '~/.config/nvim/files/dprint.json',
+          }
+        end,
+      },
     },
   },
 }
