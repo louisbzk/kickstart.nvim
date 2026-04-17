526c1cea4918c6472d151f1264a4acc6ae7f5c2b
louis.blazejczak-boulegue <louis.blazejczak-boulegue@sysnav.fr>
2026-04-17 11:21:18 +0200

    feat(keybindings): Telescope buffers

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -41,6 +41,7 @@ vim.keymap.set('n', '<C-b>', '<C-b>zz', { desc = 'Center cursor after moving up 
 vim.keymap.set('n', '<C-f>', '<C-f>zz', { desc = 'Center cursor after moving down full page' })
 vim.keymap.set('n', "<leader>'", '`', { noremap = true, silent = true, desc = 'Jump to mark (Backtick)' })
 vim.keymap.set('n', '<leader>fb', ':Telescope file_browser<CR>', { desc = 'Open file browser' })
+vim.keymap.set('n', '<leader>lb', ':Telescope buffers<CR>', { desc = 'Open buffer list' })
 vim.keymap.set('n', '<leader>d<CR>', ':lua require("neogen").generate()<CR>', { desc = 'Generate [D]ocstring' })
 vim.keymap.set('n', '<F4>', ':let @+ = expand("%:p")<CR>', { desc = 'Copy full path of current buffer to clipboard' })
 vim.keymap.set('n', '<leader>nh', ':Gitsigns next_hunk<CR>', { desc = 'Gitsigns [N]ext_[H]unk' })
